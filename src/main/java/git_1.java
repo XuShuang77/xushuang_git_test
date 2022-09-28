d6854faee39c903ab1b3d57d1e43752534d18399
xs <2573631028@qq.com>
2022-09-28 14:39:51 +0800

    new_branch_2_1

--- src/main/java/git_1.java
+++ src/main/java/git_1.java
@@ -5,5 +5,6 @@ public class git_1 {
         System.out.println("hello!! test3");
         System.out.println("hello!! new_branch_1");
         System.out.println("hello!! new_branch_2");
+        System.out.println(("我是新创建的第二个分支"));
     }
 }

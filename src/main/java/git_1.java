2e437297a8b9a536e9ab8574a0951895b57239e5
xs <2573631028@qq.com>
2022-09-28 13:48:57 +0800

    commit_new_branch_1

--- src/main/java/git_1.java
+++ src/main/java/git_1.java
@@ -3,5 +3,6 @@ public class git_1 {
         System.out.println("hello!! test1");
         System.out.println("hello!! test2");
         System.out.println("hello!! test3");
+        System.out.println("hello!! new_branch_1");
     }
 }
